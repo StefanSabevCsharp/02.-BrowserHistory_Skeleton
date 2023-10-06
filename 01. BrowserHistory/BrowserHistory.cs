2d760b517d55ab31f40f356c6d5340ab61b7e2fa
Stefan Sabev <stevenbg91@gmail.com>
2023-10-06 21:26:12 +0200

    Initial commit

---
+++ 01. BrowserHistory/BrowserHistory.cs
@@ -0,0 +1,120 @@
+﻿namespace _01._BrowserHistory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using _01._BrowserHistory.Interfaces;
+
+    public class BrowserHistory : IHistory
+    {
+        private List<ILink> links = new List<ILink>();
+        private HashSet<ILink> hashLinks = new HashSet<ILink>();
+
+        public int Size => links.Count;
+
+        public void Clear()
+        {
+            links = new List<ILink>();
+        }
+
+        public bool Contains(ILink link)
+        {
+            return hashLinks.Contains(link);
+        }
+
+        public ILink DeleteFirst()
+        {
+            if (links.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            ILink link = links[0];
+            links.RemoveAt(0);
+            hashLinks.Remove(link);
+            return link;
+        }
+
+        public ILink DeleteLast()
+        {
+            if (links.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            ILink link = links[links.Count - 1];
+            links.RemoveAt(links.Count - 1);
+            hashLinks.Remove(link);
+            return link;
+        }
+
+        public ILink GetByUrl(string url)
+        {
+            ILink link = links.FirstOrDefault(x => x.Url == url);
+            return link;
+        }
+
+        public ILink LastVisited()
+        {
+            if (links.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return links[links.Count - 1];
+        }
+
+        public void Open(ILink link)
+        {
+            links.Add(link);
+            hashLinks.Add(link);
+        }
+
+        public int RemoveLinks(string url)
+        {
+            ILink[] linksToRemove = links.Where(x => x.Url.ToLower().Contains(url.ToLower())).ToArray();
+            if (linksToRemove.Length == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            foreach (var link in linksToRemove)
+            {
+                links.Remove(link);
+                hashLinks.Remove(link);
+            }
+
+            return linksToRemove.Length;
+
+        }
+
+        public ILink[] ToArray()
+        {
+            ILink[] linksArray = links.ToArray();
+            linksArray = linksArray.Reverse().ToArray();
+            return linksArray;
+        }
+
+        public List<ILink> ToList()
+        {
+            return links.Reverse<ILink>().ToList();
+        }
+
+        public string ViewHistory()
+        {
+            StringBuilder sb = new StringBuilder();
+            List<ILink> history = links.Reverse<ILink>().ToList();
+            foreach (var link in history)
+            {
+                sb.AppendLine(link.ToString());
+            }
+            if (sb.Length == 0)
+            {
+                return "Browser history is empty!";
+            }
+            else
+            {
+
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ 02.DOM/DocumentObjectModel.cs
@@ -0,0 +1,279 @@
+﻿namespace _02.DOM
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Design.Serialization;
+    using System.Runtime.InteropServices.ComTypes;
+    using _02.DOM.Interfaces;
+    using _02.DOM.Models;
+
+    public class DocumentObjectModel : IDocument
+    {
+        private IHtmlElement child;
+        public DocumentObjectModel()
+        {
+            this.Root = new HtmlElement(ElementType.Document);
+            this.Root.Children.Add(new HtmlElement(ElementType.Html));
+            this.Root.Children[0].Parent = this.Root;
+            //foreach (var item in this.Root.Children[0].Children)
+            //{
+            //    item.Parent = this.Root.Children[0];
+            //}
+            this.Root.Children[0].Children.Add(new HtmlElement(ElementType.Head));
+            this.Root.Children[0].Children.Add(new HtmlElement(ElementType.Body));
+
+
+        }
+        public DocumentObjectModel(IHtmlElement root)
+        {
+            this.Root = root;
+        }
+
+        
+
+        public IHtmlElement Root { get; private set; }
+
+        public IHtmlElement GetElementByType(ElementType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IHtmlElement> GetElementsByType(ElementType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(IHtmlElement htmlElement)
+        {
+            var node = this.Root;
+            bool result = this.ContainsWithBfs(node, htmlElement);
+            return result;
+        }
+
+        private bool ContainsWithBfs(IHtmlElement node, IHtmlElement htmlElement)
+        {
+            Queue<IHtmlElement> queue = new Queue<IHtmlElement>();
+            queue.Enqueue(node);
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                if (currentNode == htmlElement)
+                {
+                    return true;
+                }
+                foreach (var child in currentNode.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+            return false;
+        }
+
+        public void InsertFirst(IHtmlElement parent, IHtmlElement child)
+        {
+           var node = this.Root;
+            this.Root = this.InsertFirstWithBfs(node, parent, child);
+        }
+
+        private IHtmlElement InsertFirstWithBfs(IHtmlElement node, IHtmlElement parent, IHtmlElement child)
+        {
+            Queue<IHtmlElement> queue = new Queue<IHtmlElement>();
+            bool found = false;
+            queue.Enqueue(node);
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                if (currentNode == parent)
+                {
+                    found = true;
+                    child.Parent = currentNode;
+                    currentNode.Children.Insert(0, child);
+                    return this.Root;
+                }
+                foreach (var item in currentNode.Children)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+            if (!found)
+            {
+                throw new InvalidOperationException();
+            }
+            return this.Root;
+        }
+
+        public void InsertLast(IHtmlElement parent, IHtmlElement child)
+        {
+            var node = this.Root;
+            this.Root = this.InsertLastWithBfs(node, parent, child);
+        }
+
+        private IHtmlElement InsertLastWithBfs(IHtmlElement node, IHtmlElement parent, IHtmlElement child)
+        {
+            Queue<IHtmlElement> queue = new Queue<IHtmlElement>();
+            bool found = false;
+            queue.Enqueue(node);
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                if (currentNode == parent)
+                {
+                    found = true;
+                    child.Parent = currentNode;
+                    currentNode.Children.Add(child);
+                    return this.Root;
+                }
+                foreach (var item in currentNode.Children)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+            if (!found)
+            {
+                throw new InvalidOperationException();
+            }
+            return this.Root;
+        }
+
+        public void Remove(IHtmlElement htmlElement)
+        {
+            
+           this.Root = this.RemoveWithBfs(this.Root, htmlElement);
+        }
+
+        private IHtmlElement RemoveWithBfs(IHtmlElement root, IHtmlElement htmlElement)
+        {
+            Queue<IHtmlElement> queue = new Queue<IHtmlElement>();
+            queue.Enqueue(root);
+            bool found = false;
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                if (currentNode == htmlElement)
+                {
+                    found = true;
+                    currentNode.Parent.Children.Remove(currentNode);
+                    return this.Root;
+                }
+                foreach (var item in currentNode.Children)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+            if (!found)
+            {
+                throw new InvalidOperationException();
+            }
+            return this.Root;
+        }
+
+        public void RemoveAll(ElementType elementType)
+        {
+            this.Root = this.RemoveAllWithBfs(this.Root, elementType);
+        }
+
+        private IHtmlElement RemoveAllWithBfs(IHtmlElement root, ElementType elementType)
+        {
+            Queue<IHtmlElement> queue = new Queue<IHtmlElement>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                if (currentNode.Type == elementType)
+                {
+                    if(currentNode.Children.Count > 0)
+                    {
+                        currentNode.Children.Clear();
+                    }
+                    currentNode.Parent.Children.Remove(currentNode);
+                }
+                foreach (var item in currentNode.Children)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+            return this.Root;
+        }
+
+        public bool AddAttribute(string attrKey, string attrValue, IHtmlElement htmlElement)
+        {
+            IHtmlElement element = this.Find(this.Root, htmlElement);
+            if (element == null)
+            {
+                throw new InvalidOperationException();
+            }
+            if (element.Attributes.ContainsKey(attrKey))
+            {
+                return false;
+            }
+            else
+            {
+                element.Attributes.Add(attrKey, attrValue);
+                return true;
+            }
+        }
+
+        public bool RemoveAttribute(string attrKey, IHtmlElement htmlElement)
+        {
+            IHtmlElement element = this.Find(this.Root,htmlElement);
+            if (element == null)
+            {
+                throw new InvalidOperationException();
+            }
+            if (element.Attributes.ContainsKey(attrKey))
+            {
+                element.Attributes.Remove(attrKey);
+                return true;
+            }
+            return false;
+        }
+
+        private IHtmlElement Find(IHtmlElement node,IHtmlElement htmlElement)
+        {
+            Queue<IHtmlElement> queue = new Queue<IHtmlElement>();
+            queue.Enqueue(node);
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                if (currentNode == htmlElement)
+                {
+                    return currentNode;
+                }
+                foreach (var item in currentNode.Children)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+            return null;
+
+        }
+
+        public IHtmlElement GetElementById(string idValue)
+        {
+            IHtmlElement element = this.FindById(this.Root, idValue);
+            return element;
+        }
+
+        private IHtmlElement FindById(IHtmlElement root, string idValue)
+        {
+            Queue<IHtmlElement> queue = new Queue<IHtmlElement>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                if (currentNode.Attributes.ContainsKey("id"))
+                {
+                    if (currentNode.Attributes["id"] == idValue)
+                    {
+                        return currentNode;
+                    }
+                }
+                foreach (var item in currentNode.Children)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+            return null;
+        }
+    }
+}
